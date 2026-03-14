ded92e8918a6533c735261e6453da560accb57a9
ahmet <ahmetboy@gmail.com>
2026-03-14 13:13:17 +0300

    tüm crud işlemleri eklendi

--- app.js
+++ app.js
@@ -4,7 +4,70 @@ async function getTodos(){
 
  const todos = await response.json()
 
- console.log(todos)
+ const list = document.getElementById("todoList")
+
+ list.innerHTML=""
+
+ todos.forEach(todo=>{
+
+  const li=document.createElement("li")
+
+  li.textContent=todo.task
+
+  const deleteBtn=document.createElement("button")
+  deleteBtn.textContent="Sil"
+  deleteBtn.onclick=()=> deleteTodo(todo.id)
+
+  const updateBtn=document.createElement("button")
+  updateBtn.textContent="Düzenle"
+  updateBtn.onclick=()=> updateTodo(todo.id)
+
+  li.appendChild(deleteBtn)
+  li.appendChild(updateBtn)
+
+  list.appendChild(li)
+
+ })
 
 }
 
+
+
+async function addTodo(){
+    const input = document.getElementById("todoInput")
+    const task = input.value
+
+    await fetch("http://localhost:3000/todos", {
+        method:"POST",
+        headers:{
+            "Content-Type":"application/json" // Backend’e gönderdiğimiz verinin JSON olduğunu söyler.
+        },
+        body: JSON.stringify({ // JavaScript objesini JSON string'e çevirir.
+            task: task
+        })
+    })
+    input.value = ""
+    getTodos()
+}
+
+async function deleteTodo(id){
+    await fetch(`http://localhost:3000/todos/${id}`, {
+        method:"DELETE"
+    })
+    getTodos()
+}
+async function updateTodo(id){
+    const newTask = prompt("Yeni görev gir")
+
+    if(!newTask) return
+    await fetch(`http://localhost:3000/todos/${id}`, {
+        method:"PUT",
+        headers:{
+            "Content-Type":"application/json"
+        },
+        body: JSON.stringify({
+            task: newTask})
+    })
+    getTodos()
+    
+}
--- server/server.js
+++ server/server.js
@@ -3,6 +3,9 @@ const cors = require('cors');
 const app = express();
 app.use(cors({origin: 'http://127.0.0.1:5500'}))
 
+// Frontend’den gelen JSON verisini okuyabilmek için gerekir. POST isteklerinde body’deki veriyi okuyabilmek için kullanılır.
+app.use(express.json())
+
 // geçici veri
 let todos = [
   { id: 1, task: "spor yap" },
@@ -11,6 +14,32 @@ let todos = [
 
 app.get("/todos" , (req, res) => {
     res.json(todos)
+})
+
+app.post("/todos", (req, res) => {
+    const newTodo = {
+        id: Date.now(),
+        task: req.body.task
+    }
+    todos.push(newTodo)
+    res.json(newTodo)
+})
+
+app.delete("/todos/:id", (req, res) => {
+    const id = parseInt(req.params.id)
+    todos = todos.filter(todo => todo.id !== id)
+    res.json({message: "Todo silindi"})
+})
+app.put("/todos/:id", (req, res) => {
+    const id = parseInt(req.params.id)
+    const newTask = req.body.task
+    const todo = todos.find(todo => todo.id === id)
+    if(todo){
+        todo.task = newTask
+        res.json(todo)
+    }else{
+        res.status(404).json({message: "Todo bulunamadı"})
+    }
 })
 
 app.listen(3000,()=>{
