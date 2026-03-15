ba0749ab53b8f9fc671138a9ebed2661b8222df4
ahmet <ahmetboy@gmail.com>
2026-03-15 14:16:43 +0300

    bütün backend işlemlerinin server.js dosyasında olduğu ve crud işlemleri ile postgre bağlantısı yapılmıştır.

--- server/server.js
+++ server/server.js
@@ -1,45 +1,96 @@
 const express = require('express');
 const cors = require('cors');
 const app = express();
+
+app.use(express.json()) // Frontend’den gelen JSON verisini okuyabilmek için gerekir. POST isteklerinde body’deki veriyi okuyabilmek için kullanılır.
+
 app.use(cors({origin: 'http://127.0.0.1:5500'}))
 
-// Frontend’den gelen JSON verisini okuyabilmek için gerekir. POST isteklerinde body’deki veriyi okuyabilmek için kullanılır.
-app.use(express.json())
+const {Pool} = require("pg")
 
-// geçici veri
-let todos = [
-  { id: 1, task: "spor yap" },
-  { id: 2, task: "kitap oku" }
-]
-
-app.get("/todos" , (req, res) => {
-    res.json(todos)
+const pool = new Pool({
+    user: "postgres",
+    host: "localhost",
+    database: "todoapp",
+    password: "Ahmet123.?!182106*/#",
+    port: 5432
 })
 
-app.post("/todos", (req, res) => {
-    const newTodo = {
-        id: Date.now(),
-        task: req.body.task
-    }
-    todos.push(newTodo)
-    res.json(newTodo)
+
+
+
+
+app.get("/todos", async (req,res)=>{
+
+ try{
+
+  const result = await pool.query(
+   "SELECT * FROM todos ORDER BY id"
+  )
+
+  res.json(result.rows)
+
+ }catch(err){
+  console.error(err)
+ }
+
 })
 
-app.delete("/todos/:id", (req, res) => {
-    const id = parseInt(req.params.id)
-    todos = todos.filter(todo => todo.id !== id)
-    res.json({message: "Todo silindi"})
+app.post("/todos", async (req,res)=>{
+
+ try{
+
+  const task = req.body.task
+
+  const result = await pool.query(
+   "INSERT INTO todos(task) VALUES($1) RETURNING *",
+   [task]
+  )
+
+  res.json(result.rows[0])
+
+ }catch(err){
+  console.error(err)
+ }
+
 })
-app.put("/todos/:id", (req, res) => {
-    const id = parseInt(req.params.id)
-    const newTask = req.body.task
-    const todo = todos.find(todo => todo.id === id)
-    if(todo){
-        todo.task = newTask
-        res.json(todo)
-    }else{
-        res.status(404).json({message: "Todo bulunamadı"})
-    }
+
+app.delete("/todos/:id", async (req,res)=>{
+
+ try{
+
+  const id = req.params.id
+
+  await pool.query(
+   "DELETE FROM todos WHERE id=$1",
+   [id]
+  )
+
+  res.json({message:"Todo silindi"})
+
+ }catch(err){
+  console.error(err)
+ }
+
+})
+app.put("/todos/:id", async (req,res)=>{
+
+ try{
+
+  const id = req.params.id
+  const task = req.body.task
+
+  const result = await pool.query(
+   "UPDATE todos SET task=$1 WHERE id=$2 RETURNING *",
+   [task,id]
+  )
+
+  res.json(result.rows[0])
+
+ }catch(err){
+  console.error(err)
+ }
+
 })
 
 app.listen(3000,()=>{
